753fd68dec0cf7108bf9f65ff58f7e58f942a76c
Luana Lacerda <luanaretifica@hotmail.com>
2021-08-10 20:22:23 -0300

    Cod para ajudar seu irmão mais novo a contar a partir do 21

---
+++ esconde-esconde.js
@@ -0,0 +1,13 @@
+function solucao(numero) {
+
+    let numeros = 21
+ while (numeros <= numero){
+     console.log(numeros)
+     numeros ++
+     
+     } if (numero <= 20){
+         console.log("VOCE CONSEGUE, MANINHO")
+     }
+    
+
+}
